121067d8fc6390e0157ab7ea7a69ebcba3a3fac7
Axel Bellec <axel.bellec@outlook.fr>
2017-09-29 09:19:00 +0200

    fix typo

--- scripts/merge_train_test.py
+++ scripts/merge_train_test.py
@@ -2,8 +2,8 @@ import pandas as pd
 
 SAMPLE = True
 
-train_filepath = os.path.join('data', 'train.csv') if NOT SAMPLE else os.path.join('data', 'train_sample.csv')
-test_filepath = os.path.join('data', 'test.csv') if NOT SAMPLE else os.path.join('data', 'test_sample.csv')
+train_filepath = os.path.join('data', 'train.csv') if not SAMPLE else os.path.join('data', 'train_sample.csv')
+test_filepath = os.path.join('data', 'test.csv') if not SAMPLE else os.path.join('data', 'test_sample.csv')
 
 train = pd.read_csv(train_filepath, sep=';')
 test = pd.read_csv(test_filepath)
