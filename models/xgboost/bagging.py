e8406b587ea03e304a47565a6421f1d6d41491ea
Max Halford <maxhalford25@gmail.com>
2017-09-29 10:51:57 +0200

    Do bagging

--- models/xgboost/bagging.py
+++ models/xgboost/bagging.py
@@ -8,34 +8,35 @@ from sklearn.externals import joblib
 
 
 # Load data
-X_train = pd.read_csv('data/X_train.csv')
-y_train = pd.read_csv('data/y_train.csv')
-X_test = pd.read_csv('data/X_test.csv')
-y_test = pd.read_csv('data/y_test.csv')
+X_train = pd.read_csv('data/X_train.csv', sep=';')
+y_train = pd.read_csv('data/y_train.csv', sep=';')
+X_test = pd.read_csv('data/X_test.csv', sep=';')
+y_test = pd.read_csv('data/y_test.csv', sep=';')
 
 # Create a validation set with 20% of the training set
 X_fit, X_val, y_fit, y_val = model_selection.train_test_split(X_train, y_train, test_size=0.2)
 
 # Load the tuned pipeline
-pipeline = joblib.load('pipelines/xgboost/pipeline.pkl')
+pipeline = joblib.load('models/xgboost/pipeline.pkl')
 
 predictions = []
 
 n = 10
 for i in range(n):
-    pipeline.set_params(random_seed=random.randint(1, 10e9))
+    pipeline.set_params(gbm__seed=random.randint(1, 10e9))
     pipeline.fit(
         X_fit,
-        y_fit['Survived'],
-        eval_set=[X_val, y_val['Survived']]
+        y_fit['OrderQty'],
+        gbm__eval_set=[(X_val, y_val['OrderQty'])]
     )
-    fit_score = metrics.accuracy_score(y_fit['Survived'], pipeline.predict(X_fit))
-    val_score = metrics.accuracy_score(y_val['Survived'], pipeline.predict(X_val))
+    fit_score = metrics.accuracy_score(y_fit['OrderQty'], pipeline.predict(X_fit))
+    val_score = metrics.accuracy_score(y_val['OrderQty'], pipeline.predict(X_val))
     print('Model {} / {}, fit_score: {:.5f}, val_score: {:.5f}'.format(i+1, n, fit_score, val_score))
-    predictions.append(pipeline.predict_proba(X_test)[:, 1])
+    predictions.append(pipeline.predict(X_test))
 
+submission = pd.DataFrame(data={
+    'id': range(len(y_test)),
+    'demand': np.mean(predictions, axis=0)
+}).sort_values('id')
 
-y_pred = np.rint(np.mean(predictions, axis=0)).astype(int)
-
-submission = pd.DataFrame({'PassengerId': y_test['PassengerId'], 'Survived': y_pred})
 submission.to_csv('models/xgboost/submission_xgboost_bagged.csv', index=False)
--- models/xgboost/predict.py
+++ models/xgboost/predict.py
@@ -7,10 +7,10 @@ y_test = pd.read_csv('data/y_test.csv', sep=';')
 
 pipe = joblib.load('models/xgboost/pipeline.pkl')
 
-y_pred = pipe.predict_proba(X_test)
+y_pred = pipe.predict(X_test)
 
 submission = pd.DataFrame(data={
-    'id': y_test['ID'].astype(int),
+    'id': range(len(y_pred)),
     'demand': y_pred
 }).sort_values('id')
 
