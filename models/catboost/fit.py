9c0930031e89649d73d58b89e640d3cadf26a0ba
Axel Bellec <axel.bellec@outlook.fr>
2017-09-29 19:17:17 +0200

    add features

--- models/catboost/fit.py
+++ models/catboost/fit.py
@@ -1,36 +1,30 @@
 import json
 
-from catboost import CatBoostClassifier
+from catboost import CatBoostRegressor
 import numpy as np
 import pandas as pd
 from sklearn import model_selection
+from sklearn import pipeline
 from sklearn.externals import joblib
 
 
-# Load the best params
-params = json.load(open('models/catboost/params.json'))
-
-# Instantiate model
-model = CatBoostClassifier().set_params(**params)
-
-# Load data
-X_train = pd.read_csv('data/X_train.csv')
-y_train = pd.read_csv('data/y_train.csv')['Survived']
-
-# Determine categorical features
-cat_features = np.where(X_train.dtypes == int)[0]
+X_train = pd.read_csv('data/X_train.csv', sep=';')
+y_train = pd.read_csv('data/y_train.csv', sep=';')['OrderQty']
 
 # Create a validation set with 20% of the training set
 X_fit, X_val, y_fit, y_val = model_selection.train_test_split(X_train, y_train, test_size=0.2)
 
+
 # Fit the model on the training set
-model.fit(
+pipe = pipeline.Pipeline([
+    ('cat', CatBoostRegressor(learning_rate=0.05, depth=8, loss_function='MAE'))
+])
+pipe.fit(
     X_fit,
     y_fit,
-    eval_set=[X_val, y_val],
-    cat_features=cat_features,
+    eval_set=[(X_fit, y_fit), (X_val, y_val)],
     verbose=True
 )
 
 # Save the fitted model
-joblib.dump(model, 'models/catboost/model.pkl')
+joblib.dump(pipe, 'models/catboost/pipeline.pkl')
