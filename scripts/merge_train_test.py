10332ca6d3402310eb52606e0fa0cf0f8f7d53b6
Max Halford <maxhalford25@gmail.com>
2017-09-29 09:14:13 +0200

    Fix merge_train_test

--- scripts/merge_train_test.py
+++ scripts/merge_train_test.py
@@ -4,7 +4,7 @@ import pandas as pd
 train = pd.read_csv('data/train.csv', sep=';')
 test = pd.read_csv('data/test.csv')
 
-train = train[['ID', 'SalOrg', 'Material', 'Month']]
+train = train[['ID', 'SalOrg', 'Material', 'Month', 'OrderQty']]
 
 merged = pd.concat((train, test), axis='rows')
 merged.to_csv('data/merged.csv', index=False)
