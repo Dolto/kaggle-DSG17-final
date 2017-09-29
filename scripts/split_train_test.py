006722a15a87f6807c7257fdf20e4ea1587c3231
Max Halford <maxhalford25@gmail.com>
2017-09-29 10:07:24 +0200

    Fix split train test

--- scripts/split_train_test.py
+++ scripts/split_train_test.py
@@ -8,8 +8,8 @@ train_idxs = features['OrderQty'].notnull()
 train = features[train_idxs]
 test = features[~train_idxs]
 
-train.drop(['OrderQty', 'ID'], axis='columns').to_csv('data/X_train.csv', index=False, sep=';')
+train.drop(['OrderQty', 'ID', 'Material', 'SalOrg'], axis='columns').to_csv('data/X_train.csv', index=False, sep=';')
 train[['OrderQty', 'ID']].to_csv('data/y_train.csv', index=False, sep=';')
 
-test.drop(['OrderQty', 'ID'], axis='columns').to_csv('data/X_test.csv', index=False, sep=';')
+test.drop(['OrderQty', 'ID', 'Material', 'SalOrg'], axis='columns').to_csv('data/X_test.csv', index=False, sep=';')
 test[['OrderQty', 'ID']].to_csv('data/y_test.csv', index=False, sep=';')
